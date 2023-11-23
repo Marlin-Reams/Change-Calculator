3024afe6d5cae91fa7e08478122275a637deaa15
Marlin Reams <marlinreams@gmail.com>
2023-11-23 18:06:56 -0500

    Penny and Nickle functionality done

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,15 @@
+
 import React, { useState } from "react";
+
+
 
 
 
 function Calculator() {
     const[Pennies, setPennies] = useState(0);
+    const[PennyRolls, setPennyRolls] = useState(0);
     const[Nickles, setNickles] = useState(0);
+    const[NickleRolls, setNickleRolls] = useState(0);
     const[Dimes, setDimes] = useState(0);
     const[Quarters, setQuarters] = useState(0);
     const[Ones, setOnes] = useState(0);
@@ -41,6 +46,39 @@ function Calculator() {
         return totalValue.toFixed(2);
     };
 
+    const calculateChange = () => {
+      const PennyRollAmount = PennyRolls * 50;
+      const TotalPennies = parseInt(Pennies) + parseInt(PennyRollAmount)
+      const NickleRollAmount = NickleRolls * 20
+      const TotalNickles = parseInt(NickleRollAmount) + parseInt(Nickles)
+
+
+      const NeededPennies = 200 - TotalPennies;
+
+
+      if (PennyRollAmount > 0 || Pennies > 0) {
+        const NeededPennyRolls = Math.floor(NeededPennies / 50);
+
+        if (NeededPennyRolls > 0) {
+          alert(`Get ${NeededPennyRolls} rolls of pennies`);
+        }
+      }
+
+      const NeededNickles = 120 - TotalNickles;
+
+      if (NickleRollAmount > 0 || Nickles > 0) {
+        alert(NeededNickles)
+        const NeededNickleRolls = Math.floor(NeededNickles / 40);
+
+        if (NeededNickleRolls > 0) {
+          alert(`Get ${NeededNickleRolls} rolls of Nickles`)
+        }
+      }
+    };
+
+
+
+
 
     return(
         <div>
@@ -51,6 +89,14 @@ function Calculator() {
           value={Pennies}
           onChange={(e) => setPennies(e.target.value)}
         />
+        </label>
+        <label>
+        Penny Rolls:
+        <input
+          type="number"
+          value={PennyRolls}
+          onChange={(e) => setPennyRolls(e.target.value)}
+        />
       </label>
       <label>
         Nickels:
@@ -58,6 +104,14 @@ function Calculator() {
           type="number"
           value={Nickles}
           onChange={(e) => setNickles(e.target.value)}
+        />
+      </label>
+      <label>
+        NickelsRolls:
+        <input
+          type="number"
+          value={NickleRolls}
+          onChange={(e) => setNickleRolls(e.target.value)}
         />
       </label>
       <label>
@@ -127,9 +181,13 @@ function Calculator() {
       <button onClick={() => alert(`Total Value: $${calculateTotal()}`)}>
       Calculate total
       </button>
+      <button onClick={() => alert(`Total Value: $${calculateChange()}`)}>
+      Calculate Change
+      </button>
     </div>
     )
 }
+
 
 
 export default Calculator;
